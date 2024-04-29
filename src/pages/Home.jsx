fac407e44cdce46fb6592872be0fe1bbddb5d30e
Медруг <i.n.kustov@gmail.com>
2024-04-29 16:43:02 +0500

    homepage rewrite started

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,17 @@
+import ultrasound from '../img/home/ultrasound.jpg';
+import endoscopy from '../img/home/endoscopy.jpg';
+
 const Home = () => {
   return (
-    <div>
-      <div><p>Homehome</p></div>
+    <div style={{display:'flex', justifyContent:'space-evenly'}} >
+      <div style={{display:'flex', flexDirection:'column', width:160}} >
+        <img style={{objectFit:'cover', margin:20}} src={ultrasound} />
+        <p style={{textAlign:'center'}}>УЛЬТРАЗВУКОВЫЕ СИСТЕМЫ</p>
+      </div>
+      <div style={{display:'flex', flexDirection:'column', width:160}} >
+        <img style={{objectFit:'cover', margin:20}} src={endoscopy} />
+        <p style={{textAlign:'center'}}>ЭНДОСКОПИЧЕСКИЕ СИСТЕМЫ</p>
+      </div>
     </div>
   );
 }
