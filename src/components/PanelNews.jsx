dfda31bc0f8104d8fcc55e7e9fcf51e8a42565e0
MedRug <i.kustov@medrug.kz>
2024-05-03 12:37:31 +0500

    fixes

--- src/components/PanelNews.jsx
+++ src/components/PanelNews.jsx
@@ -1,30 +1,36 @@
 import { useState } from 'react';
+import { useMemo } from 'react';
 
 const PanelNews = ({caption, image, children}) => {
   const [isHovered, setHovered] = useState(false);
   const [isFolded, setFolded] = useState(true);
 
-  const style = () => ({
-    display: 'flex',
-    flexDirection: isFolded ? 'row' : 'column',
-    alignItems: isFolded ? 'flex-start' : 'center',
-    height: isFolded ? 200 : 'auto',
-    flex: isFolded ? '1 0 400px' : '0 0 100%',
-    overflow: 'hidden',
-    color: isHovered && isFolded ? 'white' : 'black',
-    backgroundColor: isHovered && isFolded ? 'lightseagreen' : 'transparent',
-    transition: '.3s',
-    cursor: 'pointer',
-    margin: '2%'
-  })
-  
-  const imgStyle = () => ({
-    width: isFolded ? 200 : '40%',
-    height: isFolded ? 200 : 'auto',
-    objectFit: 'cover',
-    transition: '.3s'
-  })
+  const style = useMemo(
+    () => ({
+      display: 'flex',
+      flexDirection: isFolded ? 'row' : 'column',
+      alignItems: isFolded ? 'flex-start' : 'center',
+      height: isFolded ? 200 : 'auto',
+      flex: isFolded ? '1 0 400px' : '0 0 100%',
+      overflow: 'hidden',
+      color: isHovered && isFolded ? 'white' : 'black',
+      backgroundColor: isHovered && isFolded ? 'lightseagreen' : 'transparent',
+      transition: '.3s',
+      cursor: 'pointer',
+      margin: '2%'
+    }),
+    [isHovered, isFolded]
+  )
 
+  const imgStyle = useMemo(
+    () => ({
+      width: isFolded ? 200 : '40%',
+      height: isFolded ? 200 : 'auto',
+      objectFit: 'cover',
+      transition: '.3s'
+    }),
+    [isFolded]
+)
   const handleFold = () => {
     setFolded(isFolded => !isFolded);
   };
@@ -34,9 +40,9 @@ const PanelNews = ({caption, image, children}) => {
       onMouseEnter={() => setHovered(true)}
       onMouseLeave={() => setHovered(false)}
       onClick={handleFold}
-      style={style()}
+      style={style}
     >
-      <img style={imgStyle()} src={image} alt={caption} />
+      <img style={imgStyle} src={image} alt={caption} />
       <div style={{margin: 20, textAlign: 'center'}} >
         <h3>{caption}</h3><br />
         {children}
