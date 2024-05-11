65f8a3bf9301025356d7e7fcad8acfad5e1126b3
Пользователь <i.n.kustov@gmail.com>
2024-05-11 15:14:07 +0500

    server is working

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,19 @@
 const express = require("express");
 const app = express();
+const fs = require("fs");
+const db = [
+    {id: "aspirator", tags: ["Эндоскопия"]},
+    {id: "e2", tags: ["УЗИ"]},
+]
+
+for (let i in db) {
+    fs.readFile("products/html/" + db[i].id + ".html", function(error, data){
+        if (error) return console.log(error);
+        db[i].html = data.toString();
+    });
+}
+
 app.get("/api", (req, res) => {
-    res.json({"key1": ["val1", "val2", "val3"]});
+    res.json(db);
 });
 app.listen(5000, () => {console.log("server listen 5000")});
