b37c57ea8b77f0a24edbb0f23009d410e33e505d
MedRug <i.kustov@medrug.kz>
2024-06-25 10:48:46 +0500

    responsive banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,3 +1,6 @@
+import { useResize } from "./ResizeProvider";
+import { useMemo } from 'react';
+
 const style = {
   width: '100%',
   display: 'flex',
@@ -5,16 +8,20 @@ const style = {
   alignItems: 'center'
 }
 
-const captionStyle = {
-  position: 'absolute',
-  color: 'transparent',
-  backgroundImage: 'linear-gradient(90deg, whitesmoke 0%, white 45%, transparent 50%, whitesmoke 55%, whitesmoke 100%)',
-  backgroundSize: '200% 100%',
-  backgroundClip: 'text',
-  animation: 'moveGradient 1.5s linear infinite',
-}
-
 const Banner = ({children, image}) => {
+  const isMobile = useResize();
+
+  const captionStyle = useMemo(() => ({
+    position: 'absolute',
+    color: 'transparent',
+    backgroundImage: 'linear-gradient(90deg, whitesmoke 0%, white 45%, transparent 50%, whitesmoke 55%, whitesmoke 100%)',
+    backgroundSize: '200% 100%',
+    backgroundClip: 'text',
+    animation: 'moveGradient 1.5s linear infinite',
+    textAlign: 'center',
+    fontSize: isMobile ? 32 : 64,
+  }), [isMobile] )
+
   return(
     <div style={style}>
       <img src={image} style={{width: '100%'}} alt='banner' />
